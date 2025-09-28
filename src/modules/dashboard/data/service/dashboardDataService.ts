d60b6287f0366d9d5747e1d81a82249b20d40a74
Krishnakant Parmar <krishnakantparmar2511@gmail.com>
2025-09-28 22:08:14 +0530

    minor changes

--- src/modules/dashboard/data/service/dashboardDataService.ts
+++ src/modules/dashboard/data/service/dashboardDataService.ts
@@ -2,7 +2,7 @@ import { API_BASE_URL } from "@/config/appConfig";
 
 export async function fetchDashboardDataService() {
   const res = await fetch(`${API_BASE_URL}/api/dashboarddata`, {
-    cache: "no-store", // optional: avoid Next.js caching issues
+    cache: "no-store", 
   });
   if (!res.ok) {
     throw new Error("Failed to fetch dashboard data");
--- src/modules/timesheet/data/service/timeSheetService.ts
+++ src/modules/timesheet/data/service/timeSheetService.ts
@@ -1,6 +1,6 @@
 import { API_BASE_URL } from "@/config/appConfig";
 
-// services/timesheetService.ts
+
 export async function fetchTimesheetByWeekId(weekId: string) {
   const res = await fetch(`${API_BASE_URL}/api/timesheet?weekId=${weekId}`);
   if (!res.ok) throw new Error("Failed to fetch timesheet data");
